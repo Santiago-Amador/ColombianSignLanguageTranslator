e712f6586188e0b7a5df34d015545c76ead23380
USUARIO <santiago.amador-d@mail.escuelaing.edu.co>
2025-11-19 11:05:34 -0500

    Initial commit - proyecto traductor lenguaje de señas Colombiano

---
+++ src/main/java/java/edu/eci/ptia/ColombianSignLanguageTranslator/ColombianSignLanguageTranslatorApplication.java
@@ -0,0 +1,13 @@
+package java.edu.eci.ptia.ColombianSignLanguageTranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombianSignLanguageTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColombianSignLanguageTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/edu/eci/ptia/ColombianSignLanguageTranslator/ColombianSignLanguageTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package java.edu.eci.ptia.ColombianSignLanguageTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColombianSignLanguageTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
